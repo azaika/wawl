b201d4c31a4c700a5bb45d110a02bca8c42bbc13
azaika <takaaza@googlemail.com>
2016-03-14 01:31:59 +0900

    Add getFileSize, readFile, writeFile and minor changes

--- wawl/Console.hpp
+++ wawl/Console.hpp
@@ -2,17 +2,20 @@
 #define ENABLE_WAWL_CONSOLE
 
 #include "BaseType.hpp"
+#include "SecurityObjects.hpp"
 
 namespace wawl {
-	namespace console {
-		
-		// console background color
-		enum BkgColor : Uint32 {
-			Blue = BACKGROUND_BLUE,
-			Green = BACKGROUND_GREEN,
-			Red = BACKGROUND_RED,
-			Intensity = BACKGROUND_INTENSITY
-		};
+	namespace fs {
+		namespace console {
 
-	} // ::wawl::console
+			// console background color
+			enum BkgColor : Uint32 {
+				Blue = BACKGROUND_BLUE,
+				Green = BACKGROUND_GREEN,
+				Red = BACKGROUND_RED,
+				Intensity = BACKGROUND_INTENSITY
+			};
+
+		} // ::wawl::fs::console
+	} // ::wawl::fs
 } // ::wawl
