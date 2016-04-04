8dcb9fbd2c12b837153939afdea4bf4330a2250d
azaika <takaaza@googlemail.com>
2016-04-05 01:27:56 +0900

    Add Debug.hpp and some functions

--- wawl/Process.hpp
+++ wawl/Process.hpp
@@ -5,13 +5,11 @@
 #include "ConsoleBaseType.hpp"
 #include "FileBaseType.hpp"
 #include "WindowBaseType.hpp"
+#include "ProcessBaseType.hpp"
 
 namespace wawl {
 	namespace fs {
 		namespace proc {
-
-			// information for application startup
-			using StartupInfo = ::STARTUPINFO;
 
 			// make an initialized StartupInfo
 			inline StartupInfo makeStartupInfo(
@@ -250,32 +248,6 @@ namespace wawl {
 				return si;
 			}
 			
-			// process creation provision
-			enum class ProcessCreateProv : Uint32 {
-				NotInheritJob = CREATE_BREAKAWAY_FROM_JOB,
-				NotInheritErrorMode = CREATE_DEFAULT_ERROR_MODE,
-				DosMode = CREATE_FORCEDOS,
-				CreateNewConsole = CREATE_NEW_CONSOLE,
-				CreateNewProcGroup = CREATE_NEW_PROCESS_GROUP,
-				NotCreateWindow = CREATE_NO_WINDOW,
-				VdmMode = CREATE_SEPARATE_WOW_VDM,
-				SharedVdmMode = CREATE_SHARED_WOW_VDM,
-				SuspendPrimary = CREATE_SUSPENDED,
-				DebugMode = DEBUG_PROCESS,
-				NotInheritDebugMode = DEBUG_ONLY_THIS_PROCESS,
-				DisallowAccessConsole = DETACHED_PROCESS,
-
-				PriorityLv1 = IDLE_PRIORITY_CLASS,
-				PriorityLv2 = BELOW_NORMAL_PRIORITY_CLASS,
-				PriorityLv3 = NORMAL_PRIORITY_CLASS,
-				PriorityLv4 = ABOVE_NORMAL_PRIORITY_CLASS,
-				PriorityLv5 = HIGH_PRIORITY_CLASS,
-				MaxPriority = REALTIME_PRIORITY_CLASS
-			};
-
-			// process infomation
-			using ProcessInfo = ::PROCESS_INFORMATION;
-
 			// initialize an ProcessInfo
 			inline bool createProcess(
 				ProcessInfo& procInfo,
