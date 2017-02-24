56b7c9e01e660bdfc1ac649ef1c894b84e767a39
azaika <takaaza@googlemail.com>
2017-02-24 22:45:47 +0900

    done Cursor

--- wawl/Cursor.hpp
+++ wawl/Cursor.hpp
@@ -8,69 +8,233 @@
 namespace wawl {
 	namespace wnd {
 
-		inline CursorHandle loadOEMCursor(
-			OEMCursorType type,
-			Flags<ImageLoadOption> loadOption,
-			Size size = {}
-			) {
-			return
-				static_cast<CursorHandle>(
+		class Cursor {
+		public:
+			Cursor() = default;
+			Cursor(Cursor&&) = default;
+			Cursor& operator = (Cursor&&) = default;
+
+			// uncopyable
+			Cursor(const Cursor&) = delete;
+			Cursor& operator = (const Cursor&) = delete;
+
+			// load OEM cursor with system metrix
+			Cursor(const OEMCursor& oem, Flags<ImageLoadOption> loadOption = {}) {
+				handle = static_cast<CursorHandle>(
 					::LoadImage(
 						nullptr,
-						type,
+						oem.unpack(),
 						IMAGE_CURSOR,
-						size.x,
-						size.y,
-						loadOption.get()
-						)
-					);
-		}
-
-		inline CursorHandle loadCursor(
-			ModuleHandle module,
-			const Tstring& fileName,
-			Flags<ImageLoadOption> loadOption,
-			const Size& size
+						0, 0,
+						loadOption.get() | LR_DEFAULTSIZE
+					));
+			}
+			// load OEM cursor with specified size
+			Cursor(
+				const OEMCursor& oem,
+				const Size& size,
+				Flags<ImageLoadOption> loadOption = {}
 			) {
-			return
-				static_cast<CursorHandle>(
+				handle = static_cast<CursorHandle>(
 					::LoadImage(
-						module,
-						fileName.c_str(),
+						nullptr,
+						oem.unpack(),
 						IMAGE_CURSOR,
-						size.x,
-						size.y,
+						size.x, size.y,
+						loadOption.get()
+					));
+			}
+
+			// load from file with system metrix
+			Cursor(const Tstring& path, Flags<ImageLoadOption> loadOption = {}) {
+				handle = static_cast<CursorHandle>(
+					::LoadImage(
+						nullptr,
+						path.c_str(),
+						IMAGE_CURSOR,
+						0, 0,
+						loadOption.get() | LR_LOADFROMFILE | LR_DEFAULTSIZE
+					));
+			}
+			// load from fiel with specified size
+			Cursor(
+				const Tstring& path,
+				const Size& size,
+				Flags<ImageLoadOption> loadOption = {}
+			) {
+				handle = static_cast<CursorHandle>(
+					::LoadImage(
+						nullptr,
+						path.c_str(),
+						IMAGE_CURSOR,
+						size.x, size.y,
 						loadOption.get() | LR_LOADFROMFILE
-						)
-					);
-		}
+					));
+			}
 
-		inline CursorHandle loadCursorFromResource(
-			ModuleHandle module,
-			const Tstring& instName,
-			Flags<ImageLoadOption> loadOption,
-			const Size& size = {}
+			// load from resource with system metrix
+			Cursor(
+				ModuleHandle module,
+				const Tstring& instanceName,
+				Flags<ImageLoadOption> loadOption = {}
 			) {
-			return
-				static_cast<CursorHandle>(
+				handle = static_cast<CursorHandle>(
 					::LoadImage(
 						module,
-						instName.c_str(),
+						instanceName.c_str(),
 						IMAGE_CURSOR,
-						size.x,
-						size.y,
+						0, 0,
+						loadOption.get() | LR_DEFAULTSIZE
+					));
+			}
+			// load from resource with specified size
+			Cursor(
+				ModuleHandle module,
+				const Tstring& instanceName,
+				const Size& size,
+				Flags<ImageLoadOption> loadOption = {}
+			) {
+				handle = static_cast<CursorHandle>(
+					::LoadImage(
+						module,
+						instanceName.c_str(),
+						IMAGE_CURSOR,
+						size.x, size.y,
 						loadOption.get()
-						)
-					);
-		}
+					));
+			}
 
-		inline CursorHandle copyCursor(CursorHandle cursor) {
-			return CopyCursor(cursor);
-		}
+			// load OEM cursor with system metrix
+			bool load(const OEMCursor& oem, Flags<ImageLoadOption> loadOption = {}) {
+				handle = static_cast<CursorHandle>(
+					::LoadImage(
+						nullptr,
+						oem.unpack(),
+						IMAGE_CURSOR,
+						0, 0,
+						loadOption.get() | LR_DEFAULTSIZE
+					));
 
-		inline bool destroyCursor(CursorHandle cursor) {
-			return ::DestroyCursor(cursor) != 0;
-		}
+				return isActive();
+			}
+			// load OEM cursor with specified size
+			bool load(
+				const OEMCursor& oem,
+				const Size& size,
+				Flags<ImageLoadOption> loadOption = {}
+			) {
+				handle = static_cast<CursorHandle>(
+					::LoadImage(
+						nullptr,
+						oem.unpack(),
+						IMAGE_CURSOR,
+						size.x, size.y,
+						loadOption.get()
+					));
+
+				return isActive();
+			}
+
+			// load from file
+			bool load(const Tstring& path, Flags<ImageLoadOption> loadOption = {}) {
+				handle = static_cast<CursorHandle>(
+					::LoadImage(
+						nullptr,
+						path.c_str(),
+						IMAGE_CURSOR,
+						0, 0,
+						loadOption.get() | LR_LOADFROMFILE | LR_DEFAULTSIZE
+					));
+
+				return isActive();
+			}
+			bool load(const Tstring& path, const Size& size) {
+				handle = static_cast<CursorHandle>(
+					::LoadImage(
+						nullptr,
+						path.c_str(),
+						IMAGE_CURSOR,
+						size.x, size.y,
+						LR_LOADFROMFILE
+					));
+
+				return isActive();
+			}
+			bool load(
+				const Tstring& path,
+				const Size& size,
+				Flags<ImageLoadOption> loadOption
+			) {
+				handle = static_cast<CursorHandle>(
+					::LoadImage(
+						nullptr,
+						path.c_str(),
+						IMAGE_CURSOR,
+						size.x, size.y,
+						loadOption.get() | LR_LOADFROMFILE
+					));
+
+				return isActive();
+			}
+
+			// load from resource
+			bool loadFromResource(
+				ModuleHandle module,
+				const Tstring& instanceName,
+				Flags<ImageLoadOption> loadOption = {}
+			) {
+				handle = static_cast<CursorHandle>(
+					::LoadImage(
+						module,
+						instanceName.c_str(),
+						IMAGE_CURSOR,
+						0, 0,
+						loadOption.get() | LR_DEFAULTSIZE
+					));
+
+				return isActive();
+			}
+			bool loadFromResource(
+				ModuleHandle module,
+				const Tstring& instanceName,
+				const Size& size,
+				Flags<ImageLoadOption> loadOption = {}
+			) {
+				handle = static_cast<CursorHandle>(
+					::LoadImage(
+						module,
+						instanceName.c_str(),
+						IMAGE_CURSOR,
+						size.x, size.y,
+						loadOption.get()
+					));
+
+				return isActive();
+			}
+
+			// release cursor resource
+			// don't call this function if the cursor is shared resource
+			bool destroy() {
+				return ::DestroyCursor(handle) != 0;
+			}
+
+			CursorHandle copy() {
+				return CopyCursor(handle);
+			}
+
+			bool isActive() const {
+				return handle != 0;
+			}
+			explicit operator bool() const {
+				return isActive();
+			}
+
+		private:
+			Cursor(CursorHandle h) : handle(h) {}
+
+			CursorHandle handle = 0;
+
+		};
 
 	} // ::wawl::wnd
 } // ::wawl
--- wawl/CursorBaseType.hpp
+++ wawl/CursorBaseType.hpp
@@ -10,12 +10,65 @@ namespace wawl {
 
 		using OEMCursorType = Tchar*;
 		// default cursor icon
-		struct OEMCursor {
-			static constexpr OEMCursorType Arrow = IDC_ARROW;
-			static constexpr OEMCursorType Cross = IDC_CROSS;
-			static constexpr OEMCursorType IBeam = IDC_IBEAM;
-			static constexpr OEMCursorType Wait = IDC_WAIT;
+		class OEMCursor {
+		public:
+			OEMCursor() = delete;
+			OEMCursor(const OEMCursor&) = delete;
+			OEMCursor& operator = (const OEMCursor&) = delete;
+
+			// default arrow with small hourglass
+			static const OEMCursor AppStart;
+			// default arrow
+			static const OEMCursor Arrow;
+			// cross mark
+			static const OEMCursor Cross;
+			// help mark
+			static const OEMCursor Help;
+			// Ibeam used in charactor input
+			static const OEMCursor IBeam;
+			// nix sign
+			static const OEMCursor Nix;
+			// cross arrow
+			static const OEMCursor CrossArrow;
+			// arrow for top-right and bottom-left
+			static const OEMCursor TrBlArrow;
+			// arrow for up and down
+			static const OEMCursor UDArrow;
+			// arrow for top-left and bottom-right
+			static const OEMCursor TlBrArrow;
+			// arrow for left and right
+			static const OEMCursor LRArrow;
+			// up arrow
+			static const OEMCursor UpArrow;
+			// hourglass mark
+			static const OEMCursor HourGlass;
+
+			OEMCursorType unpack() const {
+				return type;
+			}
+			OEMCursorType operator () () const {
+				return type;
+			}
+
+		private:
+			OEMCursor(OEMCursorType t) : type(t) {}
+			OEMCursorType type;
+
 		};
+
+		const OEMCursor OEMCursor::AppStart(IDC_APPSTARTING);
+	  	const OEMCursor OEMCursor::Arrow(IDC_ARROW);
+		const OEMCursor OEMCursor::Cross(IDC_CROSS);
+		const OEMCursor OEMCursor::Help(IDC_HELP);
+		const OEMCursor OEMCursor::IBeam(IDC_IBEAM);
+		const OEMCursor OEMCursor::Nix(IDC_NO);
+		const OEMCursor OEMCursor::CrossArrow(IDC_SIZEALL);
+		const OEMCursor OEMCursor::TrBlArrow(IDC_SIZENESW);
+		const OEMCursor OEMCursor::UDArrow(IDC_SIZENS);
+		const OEMCursor OEMCursor::TlBrArrow(IDC_SIZENWSE);
+		const OEMCursor OEMCursor::LRArrow(IDC_SIZEWE);
+		const OEMCursor OEMCursor::UpArrow(IDC_UPARROW);
+		const OEMCursor OEMCursor::HourGlass(IDC_WAIT);
 
 	} // ::wawl::wnd
 } // ::wawl
